c6e23029878de652e85a7e42c12055bbfd825fbc
mt8 <info@mt8.biz>
2016-04-18 18:27:55 +0900

    init

---
+++ GD2S3.sh
@@ -0,0 +1,81 @@
+#aws
+AWS_ACCESS_KEY_ID=YOUR_AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY=YOUR_AWS_SECRET_ACCESS_KEY
+AWS_DEFAULT_REGION=YOUR_AWS_DEFAULT_REGION
+S3_BUCKET_NAME=s3://YOUR_S3_BUCKET_NAME
+aws="/usr/local/bin/aws --region=${AWS_DEFAULT_REGION}"
+
+#dir
+GIT_REPO_DIR='/PATH/TO/YOUR/GIT/REPOSITORY/DIR'
+OUT_PUT_DIR='/PATH/TO/YOUR/WORK/DIR'
+WOKR_DIR='work'
+OUT_PUT_NAME='work'
+
+#target commit
+COMMIT_FROM=''
+COMMIT_TO='HEAD'
+
+command_usage() {
+ 	echo "Usage: git_deploy_to_s3.sh [arg...]"
+	echo ''
+	echo "Commands:"
+	echo "    sync      [git COMMIT ID]"
+	echo "    sync-all  GIT_REPO_DIR sync to Target S3 BUCKET (All Files)"
+}
+
+case "$1" in
+	
+	#
+	# sync
+	#
+	'sync' )
+	if [[ "$2" = '--help' ]]; then
+	  command_usage
+	else
+	
+		if [[ "$2" ]]; then
+			#echo 'TEST sync'
+			#echo "ID : $2"
+
+			#create work dir
+			mkdir ${OUT_PUT_DIR}/${WOKR_DIR}
+			
+			#move to git repo
+			cd ${GIT_REPO_DIR}
+			
+			#archive diff
+			git archive --format=zip --prefix=${OUT_PUT_NAME}/ ${COMMIT_TO} `git diff --diff-filter=D --name-only ${COMMIT_TO} $2^` -o ${OUT_PUT_DIR}/${WOKR_DIR}/${OUT_PUT_NAME}.zip
+			
+			#unzip
+			unzip -o -q ${OUT_PUT_DIR}/${WOKR_DIR}/${OUT_PUT_NAME}.zip -d ${OUT_PUT_DIR}/${WOKR_DIR}
+			
+			#s3 sync
+			${aws} s3 sync  ${OUT_PUT_DIR}/${WOKR_DIR}/${OUT_PUT_NAME}/ ${S3_BUCKET_NAME}/ --exclude ".git/*" --exclude ".gitignore" --exclude ".DS_Store" --exclude "*myconfig.php"
+			
+			#clean
+			rm -rf ${OUT_PUT_DIR}/${WOKR_DIR}
+
+		else
+
+			echo ''
+			echo 'necessary to set [git COMMIT ID]'
+			echo 'ex : [COMMAND] sync [git COMMIT ID]'
+			echo ''
+
+		fi
+		
+	fi
+	;;
+	
+	#
+	# sync-all
+	#
+	'sync-all' )
+	if [[ "$2" = '--help' ]]; then
+	  command_usage
+	else	
+		${aws} s3 sync  ${GIT_REPO_DIR}/ ${S3_BUCKET_NAME}/ --exclude ".git/*" --exclude ".gitignore"
+	fi
+	;;
+
+esac
